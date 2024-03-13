f24fc426728348733bf35de2f506ee7f30acc7c3
SYERAA007 <simhasimha4321.com>
2024-03-13 14:07:35 +0530

    first commit

--- app/serializers.py
+++ app/serializers.py
@@ -7,3 +7,9 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+        
+    # New Error handling and validation
+    def validate_title(self, value):
+        if 'bad_word' in value:
+            raise serializers.ValidationError('Bad word not allowed.')
+        return value
--- app/views.py
+++ app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from .models import *
 from .serializers import *
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.pagination import PageNumberPagination # New
+from rest_framework.pagination import PageNumberPagination 
+from rest_framework.response import Response
 
-# New Adding pagination
 class CustomPagination(PageNumberPagination):
     page_size = 2
     page_query_param = 'page_size'
@@ -22,6 +22,13 @@ class BookListCreate(generics.ListCreateAPIView):
     # permission_classes = [permissions.IsAuthenticated,]
     # Add pagination to your views
     pagination_class = CustomPagination
+
+    # New for error handling and validation
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
 
 class BookRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
