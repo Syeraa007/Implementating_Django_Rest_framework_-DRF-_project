d2b92c79d400ba38e5a939e9e0c767b20dadf8e8
SYERAA007 <simhasimha4321.com>
2024-03-13 13:08:22 +0530

    first commit

--- DRFprocedure/settings.py
+++ DRFprocedure/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'app',
     # 3rd party
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# New configuration for authentication and permissions
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- app/views.py
+++ app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import *
 from .serializers import *
+# New
+from rest_framework.authentication import TokenAuthentication
 
 class BookListCreate(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    # New
+    authentication_classes = [TokenAuthentication,]
+    permission_classes = [permissions.IsAuthenticated,]
 
 class BookRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    # New
+    authentication_classes = [TokenAuthentication,]
+    permission_classes = [permissions.IsAuthenticated,]
